ea52ebb7f701d8e0e7dde70417d8d18615e17f29
PACHECO <pachecoelitesoft@gmail.com>
2020-08-22 16:36:28 -0300

    ok

--- client/urcredit/src/app/app-routing.module.ts
+++ client/urcredit/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from "src/app/components/main/main.component";
 import { LoginComponent } from "src/app/components/login/login.component";
+import { Error404Component } from "src/app/components/error404/error404.component";
+
 
 const routes: Routes = [
-
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'main', component: MainComponent },
+  { path: '**', component: Error404Component }
 ];
 
 @NgModule({
--- client/urcredit/src/app/app.module.ts
+++ client/urcredit/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { MainComponent } from './components/main/main.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    MainComponent
+    MainComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
