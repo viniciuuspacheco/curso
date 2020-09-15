b026184930bc3e8bac50c2042d6ddc4f2492c6f9
PACHECO <pachecoelitesoft@gmail.com>
2020-09-15 07:54:07 -0300

    ok

--- client/urcredit/src/app/components/dividas/dividas.component.ts
+++ client/urcredit/src/app/components/dividas/dividas.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dividas.component.css']
 })
 export class DividasComponent implements OnInit {
-
+  dados = [];
   constructor() { }
 
   ngOnInit(): void {
+    for (let i = 1; i <= 100; i++) {
+      this.dados.push({ id: i, nome: Math.random().toString(36).substring(7), cpf: Math.floor(Math.random() * 99999999999 + 1), divida: Math.floor(Math.random() * 99999999999 + 1) });
+    }
   }
+  editar(dado) {
+    console.log(dado);
+  }
+  apagar(dado) {
+    console.log(dado);
 
+  }
 }
